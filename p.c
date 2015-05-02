09362274f2f60cb99a159ff2d907a01268b67f83
Manuel Gomes <manuelgomesg@gmail.com>
2015-05-01 23:01:22 -0430

    agregue el .c de prueba y el txt para probar

---
+++ p.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "person.h"
+
+int main()
+{
+	FILE *fp;
+	char *h[75];
+
+	fp=fopen("hola.txt", "r");
+	list hola;
+	hola=create();
+	getfriends(fp, hola);
+	printf("%s\n", hola->name );
+
+	return 0;
+}
