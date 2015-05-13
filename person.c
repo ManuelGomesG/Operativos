93884000132a3db8c9a77e0775686d3186db8150
Manuel <manuelgomesg@gmail.com>
2015-05-13 07:08:21 -0430

    tercer intento de lectura

--- person.c
+++ person.c
@@ -9,7 +9,41 @@ list create(){
 	return ppl;
 }
 
-list getfriends(FILE *fp, list l){
+list setUp(FILE *fp){ // Crea la lista de personas y guarda cada persona con su lista de amigos.
+	list lista = malloc(sizeof(list)); // Lista grande.
+	auxc1 = lista;  // Auxliar para moverse por la lista en el ciclo del else.
+	auxc2 = lista->lfriends; //Auxiliar para moverse por las listas de amigos del else.
+	list aux1 = lista->lfriends; //Auxiliar para moverse dentro de la lista de amigos.
+	list aux2 = lista->rnext; //Auxiliar que apunta la siguiente persona de la lista grande.
+	char buffer[200]; // Primer buffer (scout).
+	char *buffer2; // Buffer que seguro.
+	fscanf(fp,"%s", buffer); // Scan de la primera persona.
+	while (fscanf(fp,"%s", &buffer) != EOF){
+		if (buffer == "->"){
+			aux2 = malloc(sizeof(person)); // Crea una persona.
+			strcpy(aux2->name,buffer2); // Le coloca nombre a la persona.
+			// aux1 = aux2->lfriends;  * No se si sea necesario
+		}
+		if (buffer == "-None-"){
+			aux2->lfriends=NULL; // La persona no tiene amigos, el apuntador a su lista de amigos es NULL.
+		}
+		else {
+			while 
+
+		}
+
+	}
+
+
+
+}
+
+/*
+
+
+
+
+
 		char buffer[1024];
 		while (fscanf(fp,"%s ->", &buffer) != EOF){
 			printf("%s\n", buffer);
@@ -37,9 +71,7 @@ list getfriends(FILE *fp, list l){
 
 
 
-}
 
-/*
 	char buffer[1000];
 	while (fscanf(fp,"%s ->", buffer) != EOF){
 		strcpy(l->name , buffer);
@@ -49,7 +81,7 @@ list getfriends(FILE *fp, list l){
 			printf("amigos: \n %s\n", buffer);
 			person *auxl = l->lfriends;
 			while (fscanf(fp, "%s", buffer) == "\n"){
-				printf("%s\n", buffer);
+					printf("%s\n", buffer);
 				strcpy(auxl->name , buffer);
 				auxl->lnext = malloc(sizeof(person));
 				auxl=auxl->lnext;
--- person.h
+++ person.h
@@ -9,7 +9,7 @@ struct Person
 	char name[75];
 	struct Person *lnext; //Siguiente en la lista de amigos de una persona.
 	struct Person *lfriends; // Listas de amigos de cada persona de la lista grande.
-	struct Person *rnext; //Siguiente en la lista de listas (lista de personas) .
+	struct Person *rnext; //Siguiente en la lista de listas (lista grande) .
 };
 
 typedef struct Person *list;
