0e83668980ffb5eadf0faf0f7b1a462e98073886
Ricardo <munch_141@hotmail.com>
2015-05-19 17:45:54 -0430

    lee el archivo y construye la lista

---
+++ codigo/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "header_p1.h"
+
+main(int argc, char *argv[]) {
+	system("clear");
+	Lista l;
+
+	l = leer_entrada("./datos/entrada");
+	consultar(l);
+
+	l = destruir(l);
+	consultar(l);	
+}
---
+++ codigo/print_arg.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+main(int argc, char *argv[]) {
+	int i;
+	printf("arg = %s\n",argv[1]);
+}
