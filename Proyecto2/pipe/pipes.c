f710b6b19eeba23194e23392d27817e24e4f1ac1
Ricardo <munch_141@hotmail.com>
2015-06-20 20:22:17 -0430

    comente

--- Proyecto2/pipe/pipes.c
+++ Proyecto2/pipe/pipes.c
@@ -16,8 +16,7 @@ void main(int argc, char *argv[]) {
 		read(p[0], t, 1000);
 		printf("%s", t);
 	} else {
-		close(p[0]);
-
+		/* esto es para leer lo del archivo*/
 		FILE *fd = fopen("hola","r");
 
 		fseek(fd, 0, SEEK_END);
@@ -29,9 +28,10 @@ void main(int argc, char *argv[]) {
 		fclose(fd);
 
 		texto[tam] = 0;
-
+		/* hasta aquí*/
+		
+		close(p[0]);
 		write(p[1], texto, strlen(texto)+1);
-
 		free(texto);
 	}
 }
